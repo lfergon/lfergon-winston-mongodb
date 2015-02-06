9acfa82714a3b34bf5b948476ad326d6b9eabc36
lfergon <lfergon@gmail.com>
2015-02-06 23:04:40 +0100

    Change github address

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Exposes winston.transports.MongoDB (https://github.com/indexzero/winston-mongodb)',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/dgrabla/meteor-winston-mongodb',
+  git: 'https://github.com/lfergon/lfergon-winston-mongodb',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
