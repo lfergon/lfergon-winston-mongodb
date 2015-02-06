6dba637289070822a145ea6193669ebed2690c21
Luis <lfergon@gmail.com>
2015-02-06 23:14:15 +0100

    Update package.js
    v0.0.2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lfergon:winston-mongodb',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'Exposes winston.transports.MongoDB (https://github.com/indexzero/winston-mongodb)',
   // URL to the Git repository containing the source code for this package.
